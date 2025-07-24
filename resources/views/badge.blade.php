8099f708b4c3905071bd80ffa75a76230de0f488
Dennis Koch <info@pixelarbeit.de>
2025-07-24 16:25:34 +0200

    Code style

--- resources/views/badge.blade.php
+++ resources/views/badge.blade.php
@@ -3,10 +3,10 @@
 @endphp
 <div
     class="
-    environment-indicator
-    fi-badge fi-color fi-text-color-600
-    dark:fi-text-color-400
-"
+        environment-indicator
+        fi-badge fi-color fi-text-color-600
+        dark:fi-text-color-400
+    "
     style="{{ get_color_css_variables($color, [50, 300, 400, 600])  }}"
 >
     {{ $environment }}
